0815bde4415609d1e3fd15c3698778a8b10c8222
Sufyan Dawnak <61370340+Sufyan-Dawnak@users.noreply.github.com>
2021-08-06 11:18:27 +0530

    Update Home.php

--- Home.php
+++ Home.php
@@ -171,7 +171,7 @@
         text-align: center;
         top: 3vw;
         left: 43%;
-        font-family: "BrushScriptMT";
+        font-family: "Brush Script MT";
         font-size: 2vw;
     }
 
